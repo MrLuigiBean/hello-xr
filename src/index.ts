9b378b1820f10cb68cb9c439dc374beb1d6a6b29
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2024-04-03 20:08:11 +0800

    Making the VE responsive to window sizes

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 import { Engine } from "babylonjs";
 import { App } from "./app"
-// console.log('hello xr!');
+console.log('hello world!');
 
 const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-// const ctx = canvas.getContext('2d');
-// ctx.font = '50px Arial';
-// ctx.fillText('Hello XR', 50, 50);
 
 const engine = new Engine(canvas, true);
 
 const app = new App(engine, canvas)
 const scenePromise = app.createScene()
-// engine.runRenderLoop(() => {
-// 	scene.render();
-// })
 scenePromise.then(scene => {
 	engine.runRenderLoop(() => {
 		scene.render();
 	});
 })
+
+window.addEventListener('resize', () => {
+	engine.resize();
+});
