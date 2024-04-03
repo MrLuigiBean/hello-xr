6ab6af9f37f01db5a95281cd6e822e64bc0ab1ee
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2024-04-03 15:56:44 +0800

    Hello World TS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('hello xr!');
+// console.log('hello xr!');
+const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+ctx.font = '50px Arial';
+ctx.fillText('Hello XR', 50, 50);
